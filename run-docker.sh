766f5c40742209b2611deff96a4d9d7d44811a29
Brian Henning <henninb@msn.com>
2024-04-13 09:32:16 -0500

    wip

--- run-docker.sh
+++ run-docker.sh
@@ -8,8 +8,6 @@ docker context create remote --docker "host=ssh://henninb@192.168.10.10"
 echo docker context use remote
 docker context ls
 
-echo npm install
-npm run build
 docker build -t nginx-reverse-proxy .
 
 docker save nginx-reverse-proxy | docker --context remote load
@@ -17,7 +15,7 @@ docker save nginx-reverse-proxy | docker --context remote load
 echo export DOCKER_HOST=ssh://henninb@192.168.10.10
 export DOCKER_HOST=tcp://192.168.10.10:2375
 docker rm -f nginx-reverse-proxy
-docker run --name=nginx-reverse-proxy -h nginx-reverse-proxy --restart unless-stopped -p 3030:3030 -d nginx-reverse-proxy
+docker run --name=nginx-reverse-proxy -h nginx-reverse-proxy -h nginx-reverse-proxy --restart unless-stopped -p 443:443 -d nginx-reverse-proxy
 docker ps -a
 # docker rm -f nginx-reverse-proxy
 

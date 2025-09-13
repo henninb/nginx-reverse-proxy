244d657afd011b07bd87e6d6890aa963b7fd7246
Brian Henning <henninb@msn.com>
2025-09-13 07:45:42 -0500

    wip

--- run-docker.sh
+++ run-docker.sh
@@ -20,14 +20,14 @@ docker build -t nginx-reverse-proxy .
 # export DOCKER_HOST=tcp://192.168.10.10:2375
 # export DOCKER_HOST=ssh://192.168.10.10
 docker rm -f nginx-reverse-proxy
-docker run --name=nginx-reverse-proxy -h nginx-reverse-proxy --restart unless-stopped -p 2223:2223 -p 443:443 -d nginx-reverse-proxy
+docker run --name=nginx-reverse-proxy -h nginx-reverse-proxy --restart unless-stopped -p 443:443 -d nginx-reverse-proxy
 #
 # docker rm -f nginx-reverse-proxy
 docker commit nginx-reverse-proxy nginx-reverse-proxy
 docker save nginx-reverse-proxy | docker --context remote load
 export DOCKER_HOST=ssh://192.168.10.10
 docker rm -f nginx-reverse-proxy
-docker run --name=nginx-reverse-proxy -h nginx-reverse-proxy --restart unless-stopped -p 2223:2223 -p 443:443 -d nginx-reverse-proxy
+docker run --name=nginx-reverse-proxy -h nginx-reverse-proxy --restart unless-stopped -p 192.168.10.10:443:443 -d nginx-reverse-proxy
 docker ps -a
 
 exit 0
